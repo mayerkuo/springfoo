eae3124cc7b0dfebe280875b4da51b0925d19031
mayer <mayer@maxkit.com.tw>
2019-07-18 22:30:11 +0800

    add nesting element

--- src/main/resources/static/like_button.js
+++ src/main/resources/static/like_button.js
@@ -24,16 +24,36 @@ class LikeButton extends React.Component {
 class GenList extends React.Component {
    render() {
        return e('ul', null, [
-        e('li', {key: 1}, 'item1'),
+        e('li', {key: 1}, [
+                'item1',
+                e('button', {key: 1-1}, 'btn1')
+            ]),
         e('li', {key: 2}, 'item2'),
         e('li', {key: 3}, 'item3'),
        ]);
      }
 }
 
+class ClickAndRender extends React.Component {
+    render() {
+        return e(
+        'button',
+        {
+            onClick: () => {
+                console.log('on btn click');
+                const domContainer2 = document.querySelector('#ul_container');
+                ReactDOM.render(e(GenList), domContainer2);
+            }
+        },
+        'render it')
+    }
+}
 
 const domContainer = document.querySelector('#like_button_container');
 ReactDOM.render(e(LikeButton), domContainer);
 
-const domContainer2 = document.querySelector('#ul_container');
-ReactDOM.render(e(GenList), domContainer2);
+const domContainer_render_test = document.querySelector('#render_test');
+ReactDOM.render(e(ClickAndRender), domContainer_render_test);
+
+//const domContainer2 = document.querySelector('#ul_container');
+//ReactDOM.render(e(GenList), domContainer2);
